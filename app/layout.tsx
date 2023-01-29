7d6cb12741af736bd3b560dacb031e7f4bccb801
Dean Kemball <deankemball@gmail.com>
2023-01-29 18:53:51 +0100

    sliding header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Header from "@/components/Header";
 import NavBar from "@/components/NavBar";
 import clsx from "clsx";
 import Link from "next/link";
@@ -13,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const router = useRouter();
-  const [showHeader, setShowHeader] = useState(true);
+  const [showHeader, setShowHeader] = useState(false);
   return (
     <html lang="en">
       {/*
@@ -22,22 +21,29 @@ export default function RootLayout({
       */}
       <head />
       <body className="py-4 h-screen w-screen gap-4 flex flex-col">
-        <div
-          className={clsx(
-            showHeader ? "-translate-x-[12.5vw]" : "-translate-x-[100vw]",
-            "h-12 shrink-0 w-[125vw] flex items-center justify-between transition-transform duration-300 bg-red-300"
-          )}
-        >
-          <p className="w-[12.5vw] min-w-[12.5w] flex text-center select-none">
-            {"<"}
-          </p>
-          <h1 className="text-2xl">food blog</h1>
-          <Link
-            className="w-[12.5vw] min-w-[12.5w] flex text-center"
-            href={"/blog"}
+        <div className="overflow-x-clip">
+          <div
+            className={clsx(
+              showHeader ? "translate-x-0" : "-translate-x-[80%]",
+              "h-12 w-[125vw] flex items-center justify-center transition-transform duration-300 bg-red-300"
+            )}
           >
-            {"<"}
-          </Link>
+            <h1
+              onClick={() => setShowHeader(false)}
+              className="flex flex-1 text-2xl justify-center"
+            >
+              food blog
+            </h1>
+            <button
+              onClick={() => {
+                setShowHeader(true);
+                router.push("/");
+              }}
+              className="flex flex-[0.25] justify-center"
+            >
+              {"<"}
+            </button>
+          </div>
         </div>
         {children}
         <NavBar />
