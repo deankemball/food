dbce25113ef062d01fc2cd0cb9e706c9f10f1fea
Dean Kemball <deankemball@gmail.com>
2023-02-24 18:56:58 +0100

    make mobile friendly blog page

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -17,16 +17,16 @@ export default async function Post({ params }: { params: { slug: string } }) {
   const content = await mdToHtml(post.content || "");
 
   return (
-    <main className="flex justify-end align-start h-[calc(100vh-120px)]">
-      <div className="fixed inset-0 w-1/2 flex flex-col justify-center px-8 py-4">
+    <main className="flex flex-col md:flex-row md:justify-end align-start h-[calc(100vh-120px)]">
+      <div className="md:fixed md:inset-0 md:w-1/2 h-full flex flex-col justify-start md:justify-center items-center px-8 py-4">
         <Image
           alt={`cover image for ${post.title}`}
           src={post.coverImage}
           width={400}
           height={400}
-          className="w-full"
+          className="w-full max-w-sm md:max-w-lg"
         />
-        <div className="w-full flex flex-col bg-dark text-light">
+        <div className="w-full max-w-sm flex flex-col bg-dark text-light">
           <div className="flex gap-2 justify-between">
             <p className="text-md">{`${post.rating}/5`}</p>
             <p className="text-md">{`${post.venue}`}</p>
@@ -37,10 +37,11 @@ export default async function Post({ params }: { params: { slug: string } }) {
       <div
         className={clsx(
           markdownStyles["markdown"],
-          "flex flex-col text-light w-1/2 px-8 py-4"
+          "flex flex-col text-light w-full h-full md:w-1/2 px-8 py-4"
         )}
         dangerouslySetInnerHTML={{ __html: content }}
       />
+      <div className="h-16 md:h-32 shrink-0"></div>
     </main>
   );
 }
